97d3bf68edd3dbe8d6edb71f6ef6766de4d22040
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-09-06 20:16:55 +0330

    Add files via upload

---
+++ add-email-to-faker-libary.py
@@ -0,0 +1,9 @@
+from colorama import init, Fore
+init()
+print(Fore.RED + "Please Use Gmail Addres")
+email = input(Fore.GREEN + "Enter Your Email:")
+print(Fore.RED + "Your Password For iframe tag")
+password = input(Fore.GREEN + "Enter Your Email Password:")
+print(Fore.CYAN + "Welcome ", email, "Your Mail Is Sumbit For Fake Emails")
+print(Fore.CYAN + "For Use Your Fake Email In Your App Enter Commands: emailfake = ", email, "Your Fake Mail Was Added To Your App For Use For Evervone Your Fake Email Address Create Your site and use iframe tag For email inbox")
+print(Fore.CYAN + "Html and css code for add your fake email and password to google check github")
